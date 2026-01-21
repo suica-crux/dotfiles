638e932c23c4695c0b5945035fdcfb9bbf9c0b91
suica-crux <autumn@vipelar.com>
2026-01-21 23:33:49 +0900

    git-config

--- home.nix
+++ home.nix
@@ -31,23 +31,34 @@
   xdg.configFile."nvim".source = ./config/nvim;
   programs.fish.enable = true;
   programs.home-manager.enable = true;
-  programs.git = {
-    enable = true;
-    settings = {
-      user = {
-        user.name = "suica-crux";
-        user.email = "autumn@vipelar.com";
-      };
-      commit = {
-        gpgsign = true;
-      };
-      credential = {
-        helper = "!gh auth git-credential";
-      };
-    };
-    signing = {
-      key = "7FBA0C906284C1DB";
-      signByDefault = true;
-    };
-  };
+  # programs.git = {
+  #   enable = true;
+  #   settings = {
+  #     user = {
+  #       user.name = "suica-crux";
+  #       user.email = "autumn@vipelar.com";
+  #     };
+  #     commit = {
+  #       gpgsign = true;
+  #     };
+  #     credential = {
+  #       helper = "!gh auth git-credential";
+  #     };
+  #   };
+  #   signing = {
+  #     key = "7FBA0C906284C1DB";
+  #     signByDefault = true;
+  #   };
+  # };
+  home.file.".gitconfig".text = ''
+  [user]
+    name = suica-crux
+    email = autumn@vipelar.com
+  [commit]
+    gpgsign = true
+  [user]
+    signingKey = 7FBA0C906284C1DB
+  [credential]
+    helper = !gh auth git-credential
+  '';
 }
